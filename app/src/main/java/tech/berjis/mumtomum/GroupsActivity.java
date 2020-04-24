cbb82c29424d94be990a2a7b2230fd6399a54d90
BerjisTech <berjistechnologies@gmail.com>
2020-04-24 15:31:53 +0300

    Groups welcome screen

---
+++ app/src/main/java/tech/berjis/mumtomum/GroupsActivity.java
@@ -0,0 +1,14 @@
+package tech.berjis.mumtomum;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GroupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups);
+    }
+}
